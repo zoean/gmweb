ff06fdb12a27017ea169218d83c3d5920580634f
smoke <smokepei@gmail.com>
2020-07-21 18:45:20 +0800

    change message

--- src/components/Index/Url.vue
+++ src/components/Index/Url.vue
@@ -2,28 +2,32 @@
     <div class="main-area">
         <div class="smoke">
           <el-image :src="require('../../assets/images/baokao.png')" style="width: 100%;"></el-image>
-          <div class="text">亲爱的学员大家好，2020年注定是不安分的一年，疫情之下我们还能坚持学习改变自己，我们每个人都值得一个大大的赞! 一年一度的报名即将开始，为了帮助大家更加高效的完成报考工作，特发起此次问卷调查，望各位同学认真填写完整，以防届时影响报考进度。点击下方按钮就可以填写信息就啦!</div>
-          <el-button type="primary" round class="button" @click="urlClick">填写信息</el-button>
+          <div class="text" ref="text"></div>
         </div>
     </div>
 </template>
 
 <script>
-import { urlText } from '../../assets/js/common'
+import { textUrl } from '../../assets/js/common';
+import { getUrlMsg } from '../../request/api';
 export default {
     name: 'url',
     data() {
         return {
-           
+           messageData: {}
         }
     },
     created() {
-      
+      this.getUrlMsg();
     },
     methods: {
-      urlClick() {
-        let href = urlText(Number(this.$route.params.id));
-        window.open(href, '_blank');
+      getUrlMsg() {
+        this.$smoke_get(getUrlMsg + `/` + this.$route.params.id, {}).then(res => {
+          if(res.code == 200) {
+            this.messageData = res.data;
+            this.$refs.text.innerHTML = textUrl(this.messageData.msg);
+          }
+        })
       }
     },
     mounted() {
@@ -41,23 +45,16 @@ export default {
         width: 100vw;
         position: relative;
         .text{
-          width: 78%;
-          left: 11%;
+          width: 88%;
+          background: #fff;
+          padding: 20px;
+          border-radius: 6px;
+          left: 6%;
           font-size: .8rem;
-          color: #3952CF;
+          color: #333;
           position: absolute;
-          top: 38%;
+          top: 230px;
           line-height: 1.9;
-        }
-        .button{
-          width: 50%;
-          height: 2rem;
-          position: absolute;
-          bottom: 16%;
-          left: 25%;
-          font-size: .82rem;
-          line-height: .6rem;
-          background: #6B8AF3;
         }
       }
     }
