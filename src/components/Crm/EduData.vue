a9e6d3ff382e97c12fc9a6cb7e67d01be3f5d330
wudaogang <wangdiangang9788@sina.com>
2020-07-22 17:48:33 +0800

    ok

--- src/components/Crm/EduData.vue
+++ src/components/Crm/EduData.vue
@@ -16,9 +16,6 @@
         </el-select>
       </el-col>
       <el-col :span="4">
-        <el-input v-model="form.name" placeholder="请输入班主任姓名" size="small" style="width:90%"></el-input>
-      </el-col>
-      <el-col :span="4">
         <el-button type="primary" size="small" @click="query">查询</el-button>
       </el-col>
     </el-row>
@@ -26,9 +23,9 @@
       <el-table-column prop="classTeaName" label="姓名" width="180"></el-table-column>
       <el-table-column label="学员领取情况" width="180">
         <el-table-column label="领取总数" prop="receiveStuVO.stuNumCount"></el-table-column>
-        <el-table-column label="系统领取数" prop="receiveStuVO.systemAllStuNum" width="150"></el-table-column>
-        <el-table-column label="商城领取数" prop="receiveStuVO.receiveStuNum" width="150"></el-table-column>
-        <el-table-column label="主管分配内容" prop="receiveStuVO.leadAllStuNum" width="150"></el-table-column>
+        <el-table-column label="系统领取数" prop="receiveStuVO.systemAllStuNum" width="120"></el-table-column>
+        <el-table-column label="商城领取数" prop="receiveStuVO.receiveStuNum" width="120"></el-table-column>
+        <el-table-column label="主管分配内容" prop="receiveStuVO.leadAllStuNum" width="120"></el-table-column>
       </el-table-column>
       <el-table-column label="添加备注情况">
         <el-table-column label="学员数量" prop="addNotesVO.addNotesNum"></el-table-column>
@@ -36,18 +33,26 @@
       </el-table-column>
       <el-table-column label="外拨电话情况">
         <el-table-column label="系统">
-          <el-table-column label="拨打总数" prop="newSystemDialVO.newSystemNum"></el-table-column>
-          <el-table-column label="手机外拨" prop="newSystemDialVO.newSystemTelNum"></el-table-column>
-          <el-table-column label="手机外拨接通" prop="newSystemDialVO.newSystemOpenTelNum" width="150"></el-table-column>
-          <el-table-column label="座机外拨" prop="newSystemDialVO.newSystemSeatNum"></el-table-column>
-          <el-table-column label="座机外拨接通" prop="newSystemDialVO.newSystemOpenSeatNum" width="150"></el-table-column>
+          <el-table-column label="拨打总数" prop="dialVO.newSystemDialVO.newSystemNum"></el-table-column>
+          <el-table-column label="手机外拨" prop="dialVO.newSystemDialVO.newSystemTelNum"></el-table-column>
+          <el-table-column
+            label="手机外拨接通"
+            prop="dialVO.newSystemDialVO.newSystemOpenTelNum"
+            width="120"
+          ></el-table-column>
+          <el-table-column label="座机外拨" prop="dialVO.newSystemDialVO.newSystemSeatNum"></el-table-column>
+          <el-table-column
+            label="座机外拨接通"
+            prop="dialVO.newSystemDialVO.newSystemOpenSeatNum"
+            width="120"
+          ></el-table-column>
         </el-table-column>
         <el-table-column label="JQ">
           <el-table-column label="拨打总数" prop="dialVO.jqDialVO.jqNum"></el-table-column>
           <el-table-column label="手机外拨" prop="dialVO.jqDialVO.jqTelNum"></el-table-column>
-          <el-table-column label="手机外拨接通" prop="dialVO.jqDialVO.jqOpenTelNum" width="150"></el-table-column>
+          <el-table-column label="手机外拨接通" prop="dialVO.jqDialVO.jqOpenTelNum" width="120"></el-table-column>
           <el-table-column label="座机外拨" prop="dialVO.jqDialVO.jqSeatNum"></el-table-column>
-          <el-table-column label="座机外拨接通" prop="dialVO.jqDialVO.jqOpenSeatNum" width="150"></el-table-column>
+          <el-table-column label="座机外拨接通" prop="dialVO.jqDialVO.jqOpenSeatNum" width="120"></el-table-column>
         </el-table-column>
       </el-table-column>
       <el-table-column label="呼入情况">
@@ -56,9 +61,15 @@
         <el-table-column label="呼入人数" prop="callStuVO.callStuNum"></el-table-column>
       </el-table-column>
       <el-table-column label="通时情况">
-        <el-table-column label="总通时" prop="talkTimeVO.durationCount"></el-table-column>
-        <el-table-column label="外拨通话时长" prop="talkTimeVO.durationOutCount" width="150"></el-table-column>
-        <el-table-column label="呼入通话时长" prop="talkTimeVO.durationCallCount" width="150"></el-table-column>
+        <el-table-column label="总通时" prop="talkTimeVO.durationCount">
+          <template slot-scope="scope">{{timeReturn(scope.row.talkTimeVO.durationCount)}}</template>
+        </el-table-column>
+        <el-table-column label="外拨通话时长" prop="talkTimeVO.durationOutCount" width="120">
+          <template slot-scope="scope">{{timeReturn(scope.row.talkTimeVO.durationOutCount)}}</template>
+        </el-table-column>
+        <el-table-column label="呼入通话时长" prop="talkTimeVO.durationCallCount" width="120">
+          <template slot-scope="scope">{{timeReturn(scope.row.talkTimeVO.durationCallCount)}}</template>
+        </el-table-column>
       </el-table-column>
     </el-table>
   </el-main>
@@ -66,22 +77,31 @@
 
 <script>
 import { getTeaWork } from "../../request/api";
-import {} from "../../assets/js/common"; //公用方法
+import { timeReturn } from "../../assets/js/common"; //公用方法
 export default {
   name: "EduData",
   data() {
     return {
       form: {
-        date: "",
+        date: "1",
         name: ""
       },
       tableData: [],
       loading: false
     };
   },
-  created() {},
+  created() {
+    this.timeReturn = timeReturn;
+  },
   methods: {
     query() {
+      if (!this.form.date) {
+        this.$message({
+          message: "请选择查看时间段",
+          type: "warning"
+        });
+        return;
+      }
       let dateNow = new Date(new Date().toLocaleDateString()).getTime();
       let startTime = dateNow - this.form.date * 86400000 + "";
       let data = {
@@ -91,13 +111,17 @@ export default {
       };
 
       this.loading = true;
-      this.$smoke_post(getTeaWork, data).then(res => {
-        console.log(res);
-        if (res.code == 200) {
-          this.tableData = res.data;
-        }
-      });
-      this.loading = false;
+      this.$smoke_post(getTeaWork, data)
+        .then(res => {
+          console.log(res);
+          if (res.code == 200) {
+            this.tableData = res.data;
+          }
+          this.loading = false;
+        })
+        .catch(err => {
+          this.loading = false;
+        });
     }
   },
   mounted() {}
@@ -105,7 +129,4 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.el-form-item__content {
-  margin-left: 0 !important;
-}
 </style>
