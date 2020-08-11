cb90d030468c53cf44af5fff14c29ca35cee6fb1
smoke <smokepei@gmail.com>
2020-08-11 22:46:29 +0800

    分母不为0

--- src/components/Crm/SalesBoard.vue
+++ src/components/Crm/SalesBoard.vue
@@ -285,7 +285,11 @@ export default {
                         res.data.aveTime = timeReturn(res.data.aveTime);
                         res.data.daysTime1 = timeReturn(res.data.daysTime1);
                         res.data.daysTime2 = timeReturn(res.data.daysTime2);
-                        res.data.callOpenLv = ((res.data.callOpenStuNum / res.data.callStuNum) * 100).toFixed(2) + '%';
+                        if(res.data.callStuNum == 0) {
+                            res.data.callOpenLv = 0 + '%';
+                        }else{
+                            res.data.callOpenLv = ((res.data.callOpenStuNum / res.data.callStuNum) * 100).toFixed(2) + '%';
+                        }
                         arr.push(res.data);
                         this.list = arr;
                     }, 300);
