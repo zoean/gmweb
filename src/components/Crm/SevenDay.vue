420d4c7967579672c8b5e2520942d27d743eb376
smoke <smokepei@gmail.com>
2020-09-16 18:27:47 +0800

    fixed bug 释放数据

--- src/components/Crm/SevenDay.vue
+++ src/components/Crm/SevenDay.vue
@@ -310,11 +310,19 @@ export default {
                 list: arr
             }).then(res => {
                 if(res.code == 200) {
-                    this.$message({
-                        type: 'success',
-                        message: '释放数据成功'
-                    })
-                    this.threeDaysNoCallDataList();
+                    if(res.data.result){
+                        this.$message({
+                            type: 'success',
+                            message: '释放成功，提交的线索数量' + res.data.submitSize + '条' + '，实际释放的线索数量' + res.data.releaseSize + '条'
+                        });
+                        this.threeDaysNoCallDataList();
+                    }else{
+                        this.$message({
+                            type: 'error',
+                            message: res.data.msg
+                        });
+                        this.threeDaysNoCallDataList();
+                    }
                 }else{
                     this.$message({
                         type: 'error',
