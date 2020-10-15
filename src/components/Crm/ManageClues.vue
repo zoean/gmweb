c561855371f024c0fb72de6df692228ad1d0b06c
zoe <xiaoliyan@aliyun.com>
2020-10-15 15:51:27 +0800

    complete transferclude

--- src/components/Crm/ManageClues.vue
+++ src/components/Crm/ManageClues.vue
@@ -540,7 +540,8 @@ export default {
                             type: 'success',
                             message: '线索转移成功'
                         })
-                    }
+                    }                    
+                    this.getExteAllClueData();
                     this.transferSeatVisible = false
                 })
             }
