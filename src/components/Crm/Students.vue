311b195378a2b59e9e85fcc662b7989ec4a5b860
zoe <xiaoliyan@aliyun.com>
2020-11-05 10:52:10 +0800

    trimSearchTel update

--- src/components/Crm/Students.vue
+++ src/components/Crm/Students.vue
@@ -4,7 +4,7 @@
         <el-row :class="['people-screen', {actionHide: toggleAction, actionShow: !toggleAction, noSearch: hideSearch}]">
             
             <el-col :span="3">
-                <el-input v-model="form.tel" size="small" placeholder="请输入手机号"  class="screen-li"></el-input>
+                <el-input v-model.trim="form.tel" size="small" placeholder="请输入手机号"  class="screen-li"></el-input>
             </el-col>
 
             <el-col :span="3">
