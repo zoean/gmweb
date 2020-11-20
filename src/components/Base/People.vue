f9a7380e15570b15962ad7c5c98529e734c436c7
smoke <smokepei@gmail.com>
2020-11-20 17:56:36 +0800

    people页面新增分校字段

--- src/components/Base/People.vue
+++ src/components/Base/People.vue
@@ -1,6 +1,14 @@
 <template>
   <el-main class="index-main" id="indexMain">
-    <el-row class="people-screen">
+    <el-row :class="[
+        'people-screen',
+        {
+          actionHide: toggleAction,
+          actionShow: !toggleAction,
+          noSearch: hideSearch,
+        },
+      ]"
+      id="searchArea">
       <el-col :span="3">
         <el-cascader
           size="small"
@@ -91,13 +99,14 @@
         ></el-input>
       </el-col>
     </el-row>
-    <el-row class="people-screen">
+    <el-row class="people-screen handle-area">
       <el-col :span="3">
         <el-input
           v-model.trim="screenForm.jobNum"
           size="small"
           placeholder="请输入员工工号"
           class="screen-li"
+          clearable
         ></el-input>
       </el-col>
       <el-col :span="21">
@@ -204,7 +213,7 @@ import {
 import { add } from "lodash/fp";
 export default {
   name: "people",
-  props: ["tableHeight"],
+  props: ['tableHeight','toggleAction', 'hideSearch'],
   data() {
     return {
       userList: [],
@@ -213,9 +222,10 @@ export default {
         { prop: "name", label: "姓名" },
         { prop: "accountNumber", label: "手机号" },
         { prop: "jobStatus", label: "状态" },
+        { prop: "schoolList", label: "分校", width: 180 },
         { prop: "orgUuidList", label: "部门", width: 180 },
         { prop: "roleUuidList", label: "角色", width: 180 },
-        { prop: "hiredDate", label: "入职时间" },
+        { prop: "hiredDate", label: "入职时间", width: 120 },
       ],
       total: null, //总条目数
       totalFlag: false, //当只有一页时隐藏分页
@@ -397,6 +407,7 @@ export default {
             // 用户列表
             res.data.list.map((data) => {
               data.orgUuidList = getTextByJs(data.orgUuidList);
+              data.schoolList = getTextByJs(data.schoolList,'schoolName');
               data.roleUuidList = getTextByJs(data.roleUuidList);
               data.jobStatus = getTextByState(data.jobStatus);
               data.hiredDate = data.hiredDate.split(" ")[0];
@@ -457,9 +468,6 @@ export default {
 
 <style lang="less" scoped>
 .index-main {
-  margin-top: 24px;
-  position: relative;
-  overflow: hidden;
   #toggleSearch {
     position: absolute;
     top: -10px;
